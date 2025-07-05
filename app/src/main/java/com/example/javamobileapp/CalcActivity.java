32cdf952d43bdc871d844df4a15968d70bd46163
itzreiii <raymussenaw@gmail.com>
2025-07-05 16:50:36 +0700

    feat: Implement calculator page
    This commit introduces a new calculator page to the application.
    
    - Added a new `CalcActivity.java` to handle the logic for the calculator.
    - Created `calculator.xml` layout file for the calculator interface.
    - Updated `activity_main.xml` to include a button that navigates to the calculator page.
    - Modified `MainActivity.java` to handle the button click and start `CalcActivity`.
    - Registered `CalcActivity` in `AndroidManifest.xml`.

---
+++ app/src/main/java/com/example/javamobileapp/CalcActivity.java
@@ -0,0 +1,12 @@
+package com.example.javamobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalcActivity extends AppCompatActivity  {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculator);
+    }
+}
--- app/src/main/java/com/example/javamobileapp/MainActivity.java
+++ app/src/main/java/com/example/javamobileapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.javamobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView textViewHello;
     private Button buttonChangeText;
+    private Button buttonPageCalc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +27,8 @@ public class MainActivity extends AppCompatActivity {
                 textViewHello.setText("Button Clicked!");
             }
         });
+
+        buttonPageCalc = findViewById(R.id.buttonPageCalc);
+        buttonPageCalc.setOnClickListener(view -> startActivity(new Intent(this, CalcActivity.class)));
     }
 }
