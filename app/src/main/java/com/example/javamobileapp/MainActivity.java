290dfe3a51322e06ddf663a53dbef64291868894
itzreiii <raymussenaw@gmail.com>
2025-07-05 15:32:00 +0700

    init

---
+++ app/src/main/java/com/example/javamobileapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.javamobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewHello;
+    private Button buttonChangeText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewHello = findViewById(R.id.textViewHello);
+        buttonChangeText = findViewById(R.id.buttonChangeText);
+
+        buttonChangeText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textViewHello.setText("Button Clicked!");
+            }
+        });
+    }
+}
